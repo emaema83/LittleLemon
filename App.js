4b9ad03555d92b01b2e63055fdda91aa23ac2a8f
emaema83 <119667732+emaema83@users.noreply.github.com>
2023-01-30 16:16:31 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"> Home Page
+    <React.Fragment> Home Page
+
+      <Header> </Header>
+      <Main> </Main>
+      <Footer> </Footer>
       
-    </div>
+    </React.Fragment>
   );
 }
 
---
+++ Header.js
@@ -0,0 +1,20 @@
+return (
+    <header>
+        <head>
+            <meta name="description" content="Little Lemon restaurant in Chicago"/>
+            <meta name="og:title" content="Little Lemon Restaurant"/>
+            <meta name="og:description" content="Best restaurant in the town."/>
+            <meta name="og:image" content="/restaurantfood.jpg"/>
+        </head>
+
+        <nav>
+        <ul>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/about">About Us</a></li>
+            <li><a href="/blog">Blog</a></li>
+        </ul>
+
+        </nav>
+
+    </header>
+)
